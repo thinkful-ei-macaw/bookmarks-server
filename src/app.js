26da15335d2290741c00e110ed109c42c062b260
Trina <trinamceary@gmail.com>
2020-03-27 12:57:20 -0700

    GET, POST, and DELETE

---
+++ src/app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('./logger');
+const bookmarkRouter = require('./bookmark/bookmark-router')
+
+const app = express();
+
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+    //middleware function
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(bookmarkRouter)
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error'}}
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+});
+
+
+module.exports = app
---
+++ src/bookmark/bookmark-router.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const uuid = require('uuid/v4');
+const { isWebUri } = require('valid-url')
+const logger = require('../logger')
+
+const bookmarkRouter = express.Router()
+const bodyParser = express.json()
+
+const bookmarks = [{
+  id: 1,
+  title: 2,
+  rating : 4,
+  url: 'http://google.com',
+  desc: 'This is it',
+}]
+
+bookmarkRouter
+  .route('/bookmark')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+      const { title, rating, url, desc } = req.body;
+      //check for required feilds
+      if (!title) {
+        return res
+            .status(400)
+            .status('Title is required');
+      }
+      if (!url || !isWebUri(url)) {
+        logger.error(`Invalid url '${url}' supplied`)
+        return res
+            .status(400)
+            .status('Enter a valid url');
+      }
+
+      const id = uuid();
+      const bookmark = {
+        idPOST, 
+        title, 
+        url,
+        rating,
+        desc
+      }
+      bookmarks.push(bookmark);
+      logger.info(`Bookmark with id ${id} created`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmark/${id}`)
+      .json(bookmark)    
+  })
+
+  bookmarkRouter
+  .route('/bookmark/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark  = bookmarks.find(b => b.id == id);
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark Not Found');
+    }
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Not found');
+    }
+    bookmarks.splice(bookmarkIndex, 1);
+    logger.info(`Card with id ${id} deleted.`);
+    res
+      .status(204)
+      .end();
+  })
+
+  module.exports = bookmarkRouter
